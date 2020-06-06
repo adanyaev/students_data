94d53437045c08a613d9caffcec57a223edf5413
EgorBodrov <bodroff.alexeevich@yandex.ru>
2020-06-06 23:24:11 +0300

    Correct functions
    make return values in drawing functions and top five function

--- excelparsing.py
+++ excelparsing.py
@@ -2,9 +2,9 @@ import xlrd
 import matplotlib.pyplot as plt
 import math
 
-def parsing():
+
+def start_data_parsing(file_path):      # Принимает путь к файлу (только к одному), который нужно считать
     # "D:\\Python\\PY PROJECTS\\19pi.xlsx"
-    file_path = input()
     book = xlrd.open_workbook(file_path)
     sheet = book.sheet_by_index(book.nsheets - 1)
     data = {}
@@ -81,17 +81,15 @@ def parsing():
     return data, students
 
 
-def drawPiechart():
+def start_data_drawPiechart():              # Распределение конкурсных баллов по всем предметам
     values = sorted(main_data["Сумма конкурсных баллов"], key=int)
     colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
     slices = []
     if max(values) < 320:
-        max_score = 420
-        limits = [150, 240, 290, 320]
-        labels = ["0-150", "151-260", "261-280", "281-320"]
+        limits = [200, 250, 290, 320]
+        labels = ["0-200", "201-250", "251-290", "291-320"]
     else:
-        max_score = 320
-        limits = [190, 280, 350, 420]
+        limits = [200, 280, 350, 420]
         labels = ["0-190", "191-280", "281-350", "351-420"]
     len_ = len(values)
     j = 0
@@ -103,46 +101,42 @@ def drawPiechart():
         slices.append(counter)
     total = sum(slices)
     plt.title("Распределение конкурсных баллов")
-    plt.pie(slices, shadow=True, colors=colors, wedgeprops={"edgecolor": "black"},  autopct=lambda p: '{:.0f}'.format(p * total / 100), startangle=90)
+    plt.pie(slices, shadow=True, colors=colors, wedgeprops={"edgecolor": "black"},  autopct=lambda p: '{:.0f}'.format(p * total / 100), startangle=180)
     plt.legend(labels, loc="best")
     plt.axis('equal')
     plt.tight_layout()
     plt.show()
 
 
-
-
-def drawHystogramm(subject):
+def start_data_drawHistogramm(subject):     # Принимает название предмета (Информатика и ИКТ, Математика или Русский язык)
     plt.xlabel("Баллы ЕГЭ")
     plt.ylabel("Количество сдавших")
     plt.title("Распределение баллов дисциплины: " + subject)
-    ages = main_data[subject]
-    ages.sort(key=int)
+    axes = main_data[subject]
+    axes.sort(key=int)
+    for i in range(len(axes)):
+        axes[i] -= 0.01
     bins = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110]
-    plt.hist(ages, bins=bins, edgecolor='black')
-    plt.xlim(min(ages)-10, 110)
+    plt.hist(axes, bins=bins, edgecolor='black')
+    plt.xlim(min(axes)-10, 110)
     plt.show()
 
 
-def topFive(subject):
+def start_data_topFive(subject):            # Принимает название предмета, возвращает словарь
     students_list.sort(reverse=True, key=lambda n: n[subject])
-    print("Top 5 list in subject: " + subject)
-    print("----------------------------------")
-    for i in range(5):
-        print(students_list[i]["ФИО"] + " - " + str(students_list[i][subject]))
-    print("----------------------------------\n")
+    new_dict = {'ФИО': [], subject: []}
+    for i in range(0, 5):
+        new_dict['ФИО'].append(students_list[i]['ФИО'])
+        new_dict[subject].append(students_list[i][subject])
+    return new_dict
 
+main_data, students_list = start_data_parsing("D:\\Python\\PY PROJECTS\\19pi.xlsx")
 
-main_data, students_list = parsing()
-drawPiechart()
-#drawHystogramm("Информатика и ИКТ")
-#topFive("Математика")
-#topFive("Информатика и ИКТ")
-#topFive("Русский язык")
-#topFive("Экзамен 4")
-
-
-
+#   new_dict = start_data_topFive('Математика')
+#   new_dict содержит два ключа: ФИО и название предмета, значениями являются списки из имен и оценок, порядок сохраняется
+#   Пример получения этих данных
+#for i in new_dict:
+#   print(new_dict[i])
 
 
 
