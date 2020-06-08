f1831f4c521d30e51f0b1cd986448dbe08d003a8
Alyona Uskova <aluskova01@yandex.ru>
2020-06-08 15:06:37 +0300

    Without error of reading empty file

--- prog.py
+++ prog.py
@@ -1,8 +1,5 @@
-import pandas as pd
 import datetime
 import xlrd
-import xlwt
-import openpyxl
 #D:\19ПИ-3.xlsx - это путь к файлу
 class STUDENT(object):
     def __init__(self, name='', group='', results=[]):
@@ -16,21 +13,21 @@ def Current_student_grades(*file_names):
         try:
             xlrd.open_workbook(file_name, formatting_info=False)
         except FileNotFoundError:
-            print(str("Error of opening: " + str(file_name)))
+            print(str("The file is not found: " + str(file_name)))
             continue
         rb = xlrd.open_workbook(file_name, formatting_info=False)
         sheet = rb.sheet_by_index(0)            #выбираем активный лист
         row_number = sheet.nrows
         col_number = sheet.ncols
-        group = sheet.cell(rowx=0, colx=col_number-1).value
-        subject = sheet.cell(rowx=1, colx=col_number-1).value
-        i = 2
-        kolvo = 0
-        while sheet.cell(rowx=0, colx=i).value != 'Средний балл' and sheet.cell(rowx=1, colx=i).value != 'Middle point':
-            kolvo += 1
-            i += 1
-        k = 0
         if row_number > 0:
+            group = sheet.cell(rowx=0, colx=col_number-1).value
+            subject = sheet.cell(rowx=1, colx=col_number-1).value
+            i = 2
+            kolvo = 0
+            while sheet.cell(rowx=0, colx=i).value != 'Средний балл' and sheet.cell(rowx=1, colx=i).value != 'Middle point':
+                kolvo += 1
+                i += 1
+            k = 0
             for i in range(1, row_number):
                 #array = [0]*kolvo
                 j = 1
@@ -66,12 +63,12 @@ def Current_student_grades(*file_names):
                     Student = STUDENT(sheet.cell(rowx=i, colx=1).value, group, results)
                     students.append(Student)
         else:
-            print('Empty file' + file_name)
+            print('Empty file detected: ' + file_name)
             continue
     return students
 #arr = []
-#mytuple = ("D:\\19ПИ-3.xlsx", "D:\\19ПИ-1-2.xlsx", "D:\\vsc.xlsx")     пример входных данных
-#arr = Current_student_grades(*mytuple)                                 вызов моей функции, передаем кортеж в качестве аргумента
-#for i in range(len(arr)):                                              вывод результата работы функции
-#    print(str(arr[i].name) + " " + str(arr[i].group) + " ")
-#    print(arr[i].results)
+#mytuple = ("D:\\19ПИ-3.xlsx", "D:\\Empty.xlsx", "D:\\New.xls", "D:\\vcs.xlsx")     #пример входных данных
+#arr = Current_student_grades(*mytuple)                                 #вызов моей функции, передаем кортеж в качестве аргумента
+#for i in range(len(arr)):                                              #вывод результата работы функции
+#   print(str(arr[i].name) + " " + str(arr[i].group) + " ")
+#   print(arr[i].results)
